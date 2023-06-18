ac284402816d81d0f1b5bce7f7e9607f77c804ef
Muhammad Umer Saeed <muhammad.umer@ginkgoretail.com>
2023-06-18 18:33:30 +0500

    commiting code

---
+++ city_matching.py
@@ -0,0 +1,28 @@
+from fuzzywuzzy import fuzz
+
+def get_generic_match(fixed_name, names):
+    best_match = None
+    best_ratio = 0
+
+    for name in names:
+        ratio = fuzz.ratio(fixed_name, name)
+        print(f"the {fixed_name} matching ratio with {name} is: {ratio}")
+        if ratio > best_ratio:
+            best_ratio = ratio
+            best_match = name
+
+    if best_match and best_ratio > 80:
+        print(best_ratio)
+        generic_name = ''.join([c for c in best_match if c.isalpha()])
+        return generic_name
+
+    return None
+
+fixed_name = "chichawatni"
+names = ["lahore", "multan", "chicha", "phool watnee", "watnee chicha"]
+generic_name = get_generic_match(fixed_name, names)
+
+if generic_name:
+    print("Generic name:", generic_name)
+else:
+    print("No generic name found.")
